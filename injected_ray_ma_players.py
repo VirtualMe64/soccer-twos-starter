96f85851801727dcca342ad56f540836c76fd38e
Sammy Taubman <staubman1@gmail.com>
2026-04-16 11:17:41 -0400

    add pace batch job support

--- injected_ray_ma_players.py
+++ injected_ray_ma_players.py
@@ -1,3 +1,4 @@
+import os
 import ray
 from ray import tune
 from soccer_twos import EnvType
@@ -5,13 +6,16 @@ from soccer_twos import EnvType
 from wrappers import *
 
 NUM_ENVS_PER_WORKER = 3
+BASE_PORT = 8500
+
 
 if __name__ == "__main__":
-    ray.init()
+    os.environ["RAY_USAGE_STATS_ENABLED"] = "0"
+    ray.init(include_dashboard=False)
 
     create_rllib_env = create_rllib_env_with_wrapper(IndividualBallWrapper)
     tune.registry.register_env("Soccer", create_rllib_env)
-    temp_env = create_rllib_env({"variation": EnvType.multiagent_player})
+    temp_env = create_rllib_env({"variation": EnvType.multiagent_player, "base_port": BASE_PORT})
     obs_space = temp_env.observation_space
     act_space = temp_env.action_space
     temp_env.close()
@@ -44,6 +48,7 @@ if __name__ == "__main__":
             "env_config": {
                 "num_envs_per_worker": NUM_ENVS_PER_WORKER,
                 "variation": EnvType.multiagent_player,
+                "base_port": BASE_PORT,
             },
             "model": {
                 "fcnet_activation": "relu",
@@ -58,12 +63,12 @@ if __name__ == "__main__":
         stop={
             # "timesteps_total": 15000000,  # 15M
             # "time_total_s": 14400, # 4h
-            "time_total_s": 3600 * 10, # 1h
+            "time_total_s": 3600 * 12, # 1h
         },
         checkpoint_freq=30,
         checkpoint_at_end=True,
         local_dir="./ray_results",
-        restore="./ray_results/PPO_selfplay_1/hour2/checkpoint_000062/checkpoint-62",
+        restore="./ray_results/PPO_selfplay_1/PPO_Soccer_53fd9_00000_0_2026-04-16_00-11-22/checkpoint_000180/checkpoint-180",
     )
 
     # Gets best trial based on max accuracy across all training iterations.
