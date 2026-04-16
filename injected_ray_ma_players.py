9bd1f2caaacf441355eb1e975282d9777570c167
Sammy Taubman <staubman1@gmail.com>
2026-04-16 18:02:01 -0400

    add more experiment results

--- injected_ray_ma_players.py
+++ injected_ray_ma_players.py
@@ -63,12 +63,12 @@ if __name__ == "__main__":
         stop={
             # "timesteps_total": 15000000,  # 15M
             # "time_total_s": 14400, # 4h
-            "time_total_s": 3600 * 12, # 1h
+            "time_total_s": 3600 * 24, # 24h
         },
         checkpoint_freq=30,
         checkpoint_at_end=True,
         local_dir="./ray_results",
-        restore="./ray_results/PPO_selfplay_1/PPO_Soccer_53fd9_00000_0_2026-04-16_00-11-22/checkpoint_000180/checkpoint-180",
+        restore="./ray_results/PPO_selfplay_1/PPO_Soccer_d2636_00000_0_2026-04-16_11-06-19/checkpoint_000637/checkpoint-637",
     )
 
     # Gets best trial based on max accuracy across all training iterations.
